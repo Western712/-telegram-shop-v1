97870b27b2d919d5657b227688ab3d61e29fdcbb
Western712 <129212357+Western712@users.noreply.github.com>
2026-07-15 22:41:44 +0200

    Register product edit handler

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -6,6 +6,7 @@ from app.handlers.add_product import router as add_product_router
 from app.handlers.product_actions import router as product_actions_router
 from app.handlers.product_card import router as product_card_router
 from app.handlers.stock import router as stock_router
+from app.handlers.edit_product import router as edit_product_router
 
 
 def register_routers(dp: Dispatcher) -> None:
@@ -15,3 +16,4 @@ def register_routers(dp: Dispatcher) -> None:
     dp.include_router(product_actions_router)
     dp.include_router(product_card_router)
     dp.include_router(stock_router)
+    dp.include_router(edit_product_router)
