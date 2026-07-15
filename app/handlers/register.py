21c8896baf7bf2ae7055696da82f7d5444454e3c
Western712 <129212357+Western712@users.noreply.github.com>
2026-07-15 22:24:50 +0200

    Register product action callbacks

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -3,9 +3,11 @@ from aiogram import Dispatcher
 from app.handlers.start import router as start_router
 from app.handlers.products import router as products_router
 from app.handlers.add_product import router as add_product_router
+from app.handlers.product_actions import router as product_actions_router
 
 
 def register_routers(dp: Dispatcher) -> None:
     dp.include_router(start_router)
     dp.include_router(products_router)
     dp.include_router(add_product_router)
+    dp.include_router(product_actions_router)
