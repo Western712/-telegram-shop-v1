5310b02ad836aaaf743a76c42e4de314e308fd64
Western712 <129212357+Western712@users.noreply.github.com>
2026-07-15 22:19:50 +0200

    Export database models

---
+++ app/models/__init__.py
@@ -0,0 +1,13 @@
+from app.models.base import Base
+from app.models.product import Product
+from app.models.admin import Admin
+from app.models.category import Category
+from app.models.change_log import ChangeLog
+
+__all__ = [
+    "Base",
+    "Product",
+    "Admin",
+    "Category",
+    "ChangeLog",
+]
