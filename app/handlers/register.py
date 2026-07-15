442fe231c1a0c51089e684f0781d7bbf7f2d1b85
Western712 <129212357+Western712@users.noreply.github.com>
2026-07-15 22:39:55 +0200

    Register stock handlers

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -5,6 +5,7 @@ from app.handlers.products import router as products_router
 from app.handlers.add_product import router as add_product_router
 from app.handlers.product_actions import router as product_actions_router
 from app.handlers.product_card import router as product_card_router
+from app.handlers.stock import router as stock_router
 
 
 def register_routers(dp: Dispatcher) -> None:
@@ -13,3 +14,4 @@ def register_routers(dp: Dispatcher) -> None:
     dp.include_router(add_product_router)
     dp.include_router(product_actions_router)
     dp.include_router(product_card_router)
+    dp.include_router(stock_router)
