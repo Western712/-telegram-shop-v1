2bd8ebf02e64534719455b1076d0cf725755d69a
Western712 <129212357+Western712@users.noreply.github.com>
2026-07-15 22:39:47 +0200

    Register stock movement model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,7 @@ from app.models.product import Product
 from app.models.admin import Admin
 from app.models.category import Category
 from app.models.change_log import ChangeLog
+from app.models.stock_movement import StockMovement
 
 __all__ = [
     "Base",
@@ -10,4 +11,5 @@ __all__ = [
     "Admin",
     "Category",
     "ChangeLog",
+    "StockMovement",
 ]
