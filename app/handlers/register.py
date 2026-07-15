0ab0783e8d7873f30fc15466f2284d6e0f0f2079
Western712 <129212357+Western712@users.noreply.github.com>
2026-07-15 22:23:20 +0200

    Register product creation router

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -2,8 +2,10 @@ from aiogram import Dispatcher
 
 from app.handlers.start import router as start_router
 from app.handlers.products import router as products_router
+from app.handlers.add_product import router as add_product_router
 
 
 def register_routers(dp: Dispatcher) -> None:
     dp.include_router(start_router)
     dp.include_router(products_router)
+    dp.include_router(add_product_router)
